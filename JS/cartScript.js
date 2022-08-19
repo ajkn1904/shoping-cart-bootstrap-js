6fc5f93887a65665f339975ca5573d712889fb88
ajkn1904 <ajknishat@gmail.com>
2022-08-19 23:16:20 +0600

    update

--- JS/cartScript.js
+++ JS/cartScript.js
@@ -1,3 +1,4 @@
+// -------------function for increasing value ----------------
 
 function increaseNumbers(getById, isIncrease){
     const numberOfVaules = document.getElementById(getById);
@@ -18,6 +19,7 @@ function increaseNumbers(getById, isIncrease){
     return newValues;
 }
 
+// -------------function for decreasing value ----------------
 function getPriceUpdate(getUpdateById, perPrice, value)
 {
     const priceUpdate = document.getElementById(getUpdateById);
@@ -25,80 +27,70 @@ function getPriceUpdate(getUpdateById, perPrice, value)
     const previousPriceString = priceUpdate.innerText;
     const newPrice = perPrice * value;
     priceUpdate.innerText = newPrice;
-
-
+   
     return newPrice;
-
 }
 
-function getSubtotalUpdate( value1, value2String){
-const previousSubtotal = document.getElementById('subtotal-field');
-const value2 = parseFloat(value2String);
-const newSubtotal = value1 + value2;
-previousSubtotal.innerText = newSubtotal;
+// -------------function for price update ----------------
+function getPricetotalUpdate( value1, value2String){
+    const previousSubtotal = document.getElementById('subtotal-field');
+    const value2 = parseFloat(value2String);
+    const newSubtotal = value1 + value2;
+    previousSubtotal.innerText = newSubtotal;
 
-const previousTaxString = document.getElementById('tax-field');
-const previousTax = parseFloat(previousTaxString.innerText);
- 
-const newTax = (newSubtotal*.1).toFixed(2);
-previousTaxString.innerText = newTax;
+    const previousTaxString = document.getElementById('tax-field');
+    const previousTax = parseFloat(previousTaxString.innerText);
 
-const previousTotalString = document.getElementById('total-field');
-const previousTotal = parseFloat(previousTotalString.innerText);
+    const newTax = (newSubtotal*.1).toFixed(2);
+    previousTaxString.innerText = newTax;
 
-const newTotal = parseFloat(newSubtotal) + parseFloat(newTax);
-previousTotalString.innerText = newTotal;
+    const previousTotalString = document.getElementById('total-field');
+    const previousTotal = parseFloat(previousTotalString.innerText);
+
+    const newTotal = parseFloat(newSubtotal) + parseFloat(newTax);
+    previousTotalString.innerText = newTotal;
 
 }
-
 
 
 
 document.getElementById('iphone-number-plus').addEventListener('click',function(){
-const numberOfPhone = increaseNumbers('iphone-input-number', true);
-//const totalPrice = parseInt(numberOfPhone) * 1219;
-const perPrice = 1219;
-const totalPrice = getPriceUpdate('iphone-price',numberOfPhone, perPrice);
 
-const casePriceString = document.getElementById('case-price').innerText;
-const subTotalUpdate = getSubtotalUpdate(totalPrice, casePriceString)
+    const numberOfPhone = increaseNumbers('iphone-input-number', true);
+    const perPrice = 1219;
+    const totalPrice = getPriceUpdate('iphone-price',numberOfPhone, perPrice);
 
+    const casePriceString = document.getElementById('case-price').innerText;
+    const subTotalUpdate = getPricetotalUpdate(totalPrice, casePriceString)
 
 });
 
 document.getElementById('iphone-number-minus').addEventListener('click',function(){
-const numberOfPhone = increaseNumbers('iphone-input-number', false);
-const perPrice = 1219;
-const totalPrice = getPriceUpdate('iphone-price',numberOfPhone, perPrice);
+    const numberOfPhone = increaseNumbers('iphone-input-number', false);
+    const perPrice = 1219;
+    const totalPrice = getPriceUpdate('iphone-price',numberOfPhone, perPrice);
 
-const casePriceString = document.getElementById('case-price').innerText;
-const subTotalUpdate = getSubtotalUpdate(totalPrice, casePriceString)
+    const casePriceString = document.getElementById('case-price').innerText;
+    const subTotalUpdate = getPricetotalUpdate(totalPrice, casePriceString)
     
-    });
+});
 
 document.getElementById('case-number-plus').addEventListener('click',function(){
-const numberOfCase = increaseNumbers('case-input-number', true);
-//const totalPrice = numberOfCase * 59;
-const perPrice = 59;
-const totalPrice = getPriceUpdate('case-price', numberOfCase, perPrice);
+    const numberOfCase = increaseNumbers('case-input-number', true);
+    const perPrice = 59;
+    const totalPrice = getPriceUpdate('case-price', numberOfCase, perPrice);
 
-const phonePriceString = document.getElementById('iphone-price').innerText;
-const subTotalUpdate = getSubtotalUpdate(totalPrice, phonePriceString)
-
-    
-    });
+    const phonePriceString = document.getElementById('iphone-price').innerText;
+    const subTotalUpdate = getPricetotalUpdate(totalPrice, phonePriceString)
+  
+});
 
 document.getElementById('case-number-minus').addEventListener('click',function(){
-const numberOfCase = increaseNumbers('case-input-number', false);
-const perPrice = 59;
-const totalPrice = getPriceUpdate('case-price', numberOfCase, perPrice);
+    const numberOfCase = increaseNumbers('case-input-number', false);
+    const perPrice = 59;
+    const totalPrice = getPriceUpdate('case-price', numberOfCase, perPrice);
 
-const phonePriceString = document.getElementById('iphone-price').innerText;
-const subTotalUpdate = getSubtotalUpdate(totalPrice, phonePriceString)
-    
-    });
-
-
-
-    
-    
+    const phonePriceString = document.getElementById('iphone-price').innerText;
+    const subTotalUpdate = getPricetotalUpdate(totalPrice, phonePriceString)
+        
+});
